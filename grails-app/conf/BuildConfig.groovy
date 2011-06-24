d9c37f88d0e981a9d2a01af2a9e3a0c321d3278d
skurfuerst <skurfuerst@a60038d4-ad9c-4505-8000-0c4c45f3b681>
2011-06-24 13:29:26 +0000

    Annotation service finally works. TODO: alchemyConnector
    git-svn-id: https://obelinux.inf.tu-dresden.de/svn-students/skurfuerst/code/semantifier/trunk@55 a60038d4-ad9c-4505-8000-0c4c45f3b681

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        //mavenCentral()
+        mavenCentral()
         mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
@@ -33,8 +33,22 @@ grails.project.dependency.resolution = {
 		compile('ws.palladian:palladian:0.8-SNAPSHOT') {
 			 excludes "log4j", "stax", "jaxen", "weka", "lucene-core", "lucene-memory", "lucene-analyzers", "rome", "lingpipe", "junit", "xml-apis", "xercesImpl", "trove", "twitter4j-core", "prefuse", "bonecp", "snakeyaml", "jwbf", "boilerpipe", "nekohtml", "mysql-connector-java", "commons-validator", "h2", "commons-configuration", "slf4j-api"
 		}
+
+		// compile 'net.sf.json-lib:json-lib:2.4'
+		// However, JSON library is included in lib/ directory
+		// because http://jira.grails.org/browse/GRAILS-6147
+		// the following dependencies are needed for json-lib:
+		runtime('commons-beanutils:commons-beanutils:1.8.0')
+		runtime('commons-collections:commons-collections:3.2.1')
+		runtime('net.sf.ezmorph:ezmorph:1.0.6')
+		runtime('xml-resolver:xml-resolver:1.2')
+
+
 		compile('commons-configuration:commons-configuration:1.6')
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+
+		compile( 'org.codehaus.groovy.modules.http-builder:http-builder:0.5.1') {
+			transitive = false
+		}
     }
 
     plugins {
--- grails-app/services/semantifier/AnnotationService.groovy
+++ grails-app/services/semantifier/AnnotationService.groovy
@@ -1,5 +1,10 @@
 package semantifier
 
+
+import groovyx.net.http.RESTClient
+import net.sf.json.JSONNull
+import static groovyx.net.http.ContentType.JSON
+
 class AnnotationService {
 
 	def openCalaisConnector
@@ -18,7 +23,47 @@ class AnnotationService {
 
 		def processedAnnotations = []
 
-		println annotations
+
+		def freebaseClient = new RESTClient('http://api.freebase.com/api/service/search')
+
+		annotations.each { annotation ->
+			def response = freebaseClient.get(query: [query: annotation.entity, limit:5], contentType: JSON)
+
+			processedAnnotations << [
+				entity: annotation.entity,
+				offset: annotation.offset,
+				length: annotation.length,
+				tag: annotation.mostLikelyTagName, // TODO: work with this one later, to refine freebase results.
+				disambiguration: processPossibleResults(response.data.result) // TODO: convert to linked data results
+			]
+		}
+
 		return [language: language, entities: processedAnnotations];
+	}
+
+	private def processPossibleResults(results) {
+		def possibleResults = [];
+
+			// No results found, so we return the empty result list.
+		if (!results) return [];
+
+		results.each { result ->
+			if (!result) return;
+			if (result.class == JSONNull.class) return;
+
+			def possibleSingleResult = [
+				id: 'http://rdf.freebase.com/ns' + result.id,
+				name: result.name,
+				relevanceScore: result['relevance:score']
+			]
+
+			if (result.image && result.image.class != JSONNull.class) {
+				possibleSingleResult.imageThumbnailUri = 'http://api.freebase.com/api/trans/image_thumb' + result.image.id
+			}
+
+			possibleResults << possibleSingleResult
+		}
+
+		return possibleResults
 	}
 }
